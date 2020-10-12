fc495c8102fbdf9d581f561fe869a95e887fdf8e
xeronimus@gmail.com <sTrentwow890>
2020-10-12 14:43:02 +0200

    add slide about express

---
+++ scripts/simpleExpress.js
@@ -0,0 +1,8 @@
+const app = require('express')();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
